31dbec9840621d10acd3f9a187516f3fb3394221
Rahul Salvi <rsalvi006@me.com>
2014-07-26 13:09:57 -0700

    Finished MS5541C driver

--- Cubeception/src/RobotController/Robot/MS5541C/MS5541C.cpp
+++ Cubeception/src/RobotController/Robot/MS5541C/MS5541C.cpp
@@ -8,40 +8,99 @@
 
 #include "MS5541C.h"
 
-MS5541C::MS5541C() : _C1(0),
+MS5541C::MS5541C() : _D1(0),
+                     _D2(0),
+
+                     _W1(0),
+                     _W2(0),
+                     _W3(0),
+                     _W4(0),
+
+                     _C1(0),
                      _C2(0),
                      _C3(0),
                      _C4(0),
                      _C5(0),
-                     _C6(0)
-{
-    reset();
-}
+                     _C6(0) {}
 
-MS5541C::begin() {
-    reset();
+void MS5541C::begin() {
     getW1();
+    Serial.print(_W1); Serial.print(" ");
+    
     getW2();
+    Serial.print(_W2); Serial.print(" ");
+    
     getW3();
+    Serial.print(_W3); Serial.print(" ");
+    
     getW4();
+    Serial.print(_W4); Serial.print(" ");
+    Serial.println("");
+    
+    computeConstants();
+    
+    Serial.print(_C1); Serial.print(" ");
+    Serial.print(_C2); Serial.print(" ");
+    Serial.print(_C3); Serial.print(" ");
+    Serial.print(_C4); Serial.print(" ");
+    Serial.print(_C5); Serial.print(" ");
+    Serial.print(_C6); Serial.print(" ");
+    Serial.println("");
+    
+    Serial.println("MS5541C Initialized");
+    
 }
 
-MS5541C::reset() {
+void MS5541C::reset() {
     SPI.setDataMode(SPI_MODE0);
     SPI.transfer(0x15);
     SPI.transfer(0x55);
     SPI.transfer(0x40);
 }
 
-MS5541C::getPressure() {
-    
+void MS5541C::queueD1() {
+    reset();
+    SPI.transfer(0x0F);
+    SPI.transfer(0x40);
 }
 
-MS5541C::getTemperature() {
-    
+void MS5541C::queueD2() {
+    reset();
+    SPI.transfer(0x0F);
+    SPI.transfer(0x20);
 }
 
-MS5541C::getW1() {
+void MS5541C::readD1() {
+    SPI.setDataMode(SPI_MODE1);
+    _D1 = SPI.transfer(0x00);
+    _D1 = _D1 << 8;
+    
+    _D1 = _D1 | SPI.transfer(0x00);
+}
+
+void MS5541C::readD2() {
+    SPI.setDataMode(SPI_MODE1);
+    _D2 = SPI.transfer(0x00);
+    _D2 = _D2 << 8;
+    
+    _D2 = _D2 | SPI.transfer(0x00);
+}
+
+int16_t MS5541C::getPressure() {
+    int16_t UT1 = (8 * _C5) + 10000;
+    int16_t dT = _D2 - UT1;
+    int16_t OFF = _C2 + (((_C4 - 250) * dT) / 4096) + 10000;
+    int16_t SENS = (_C1 / 2) + (((_C3 + 200) * dT) / 8192) + 3000;
+    return ((SENS * (_D1 - OFF)) / 4096) + 1000;
+}
+
+int16_t MS5541C::getTemperature() {
+    int16_t UT1 = (8 * _C5) + 10000;
+    int16_t dT = _D2 - UT1;
+    return 200 + ((dT * (_C6 + 100)) / 2048);
+}
+
+void MS5541C::getW1() {
     reset();
     SPI.transfer(0x1D);
     SPI.transfer(0x50);
@@ -54,7 +113,7 @@ MS5541C::getW1() {
     
 }
 
-MS5541C::getW2() {
+void MS5541C::getW2() {
     reset();
     SPI.transfer(0x1D);
     SPI.transfer(0x60);
@@ -66,7 +125,7 @@ MS5541C::getW2() {
     _W2 = _W2 | SPI.transfer(0x00);
 }
 
-MS5541C::getW3() {
+void MS5541C::getW3() {
     reset();
     SPI.transfer(0x1D);
     SPI.transfer(0x90);
@@ -78,7 +137,7 @@ MS5541C::getW3() {
     _W3 = _W3 | SPI.transfer(0x00);
 }
 
-MS5541C::getW4() {
+void MS5541C::getW4() {
     reset();
     SPI.transfer(0x1D);
     SPI.transfer(0xA0);
@@ -90,7 +149,7 @@ MS5541C::getW4() {
     _W4 = _W4 | SPI.transfer(0x00);
 }
 
-MS5541C::computeConstants() {
+void MS5541C::computeConstants() {
     _C1 = _W1 >> 3 & 0x1FFF;
     _C2 = ((_W1 & 0x07) << 10) | ((_W2 >> 6) & 0x03FF);
     _C3 = (_W3 >> 6) & 0x03FF;
--- Cubeception/src/RobotController/Robot/MS5541C/MS5541C.h
+++ Cubeception/src/RobotController/Robot/MS5541C/MS5541C.h
@@ -10,13 +10,20 @@
 #define ____MS5541C__
 
 #include "Arduino.h"
+#include <SPI.h>
 
 class MS5541C {
 public:
     MS5541C();
     void begin();
     void reset();
-    uint16_t getPressure();
+    
+    void queueD1();
+    void queueD2();
+    void readD1();
+    void readD2();
+    
+    int16_t getPressure();
     int16_t getTemperature();
 private:
     void getW1();
@@ -25,6 +32,9 @@ private:
     void getW4();
     
     void computeConstants();
+    
+    int16_t _D1;
+    int16_t _D2;
     
     int16_t _W1;
     int16_t _W2;
